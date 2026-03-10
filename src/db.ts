943c693af7e8374e3d74eb33128a83898ed0404d
GrafRobert <grrobert2024@gmail.com>
2026-03-10 14:47:31 +0200

    Am adaugat SSL pentru baza de date SupaBase

--- src/db.ts
+++ src/db.ts
@@ -3,17 +3,21 @@ import dotenv from 'dotenv'
 
 dotenv.config();
 
-
 const pool = new Pool({
-    user: process.env.DB_USER,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || '5432'),
   database: process.env.DB_NAME,
+  
+  // Am adăugat această regulă de SSL obligatorie pentru Supabase / Cloud
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.connect()
-.then(() => console.log('Conexiune reușită la baza de date PostgreSQL!'))
-.catch((err) => console.error('Eroare la conectarea cu baza de date:', err));
+  .then(() => console.log('Conexiune reușită la baza de date PostgreSQL!'))
+  .catch((err) => console.error('Eroare la conectarea cu baza de date:', err));
 
 export default pool;
